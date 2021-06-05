6edcb6ad8a5627a2481ca053f2241a4bf5f8ecb1
Moamen50 <85317901+Moamen50@users.noreply.github.com>
2021-06-06 00:50:55 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
 #include "GPIO_INIT.h"
 #include "tm4c123gh6pm.h"
 
-int main(void){
+int main(void)
+{
     PortF_Init();
-    PortA_Init();
+    PortE_Init();
     PortB_Init();
 
-    u16 dist =450;
+    u16 dist =450; // dummy data to be displayed on the 3-digit seven segments
 
     while (1)
     {
         RGB_LEDs(dist);
-        OUT(4,5,0);
+        OUT(4,5,0); // data will be passed to the function in form of hundreds ,tens and digits
 
 
     }
