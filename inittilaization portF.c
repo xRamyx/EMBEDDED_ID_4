8d78b2937e829f96f16f3b2503f930018c1c2b15
OmarHisham100 <85303899+OmarHisham100@users.noreply.github.com>
2021-06-05 06:07:05 +0200

    Add files via upload

---
+++ inittilaization portF.c
@@ -0,0 +1,22 @@
+#include "stdint.h"
+#include "tm4c123gh6pm.h"
+
+
+void PortF_Init(void)
+{
+	    SYSCTL_RCGCGPIO_R |= 0x00000020;
+  while ((SYSCTL_PRGPIO_R&0x20) == 0){};
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;
+    GPIO_PORTF_CR_R |= 0x1F;
+    GPIO_PORTF_AMSEL_R &=~0x1F;
+    GPIO_PORTF_PCTL_R &=~ 0x000FFFFF;
+    GPIO_PORTF_DIR_R |= 0x0E;
+    GPIO_PORTF_AFSEL_R &=~ 0x1F;
+    GPIO_PORTF_PUR_R |= 0x11;
+    GPIO_PORTF_DEN_R |= 0x1F;
+    GPIO_PORTF_DATA_R &= ~0x0E; 
+}
+int main()
+{
+PortF_Init();
+}
