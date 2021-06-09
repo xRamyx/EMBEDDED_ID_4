f9e8a443e9d3f64effe319112361a5ce080b6b8b
Moamen50 <85317901+Moamen50@users.noreply.github.com>
2021-06-09 02:44:21 +0200

    Update Parsing.c

--- Parsing.c
+++ Parsing.c
@@ -1,8 +1,9 @@
 #include "GPIO_INIT.h"
 #include "tm4c123gh6pm.h"
+#include <string.h> 
+#include <stdlib.h>
 
-void Parsing(f32 arr[])
-{
+void Parsing(f32 arr[]) {
 	u8 c;
 	u8 flag=0;
 	u8* lat;
@@ -17,7 +18,7 @@ void Parsing(f32 arr[])
 	f32 rlat;
 	f32 rlon;
 	u8 inchar[50];
-		while(flag==0){
+		while(flag==0){ 
 			   c = UART5_InChar();
 				if(c =='$'){
 					c = UART5_InChar();
@@ -35,7 +36,6 @@ void Parsing(f32 arr[])
 											for(int i=0;i<50;i++){
 												inchar[i]= UART5_InChar();
 											}
-											flag==1;
 										}
 									}
 								}
@@ -43,7 +43,34 @@ void Parsing(f32 arr[])
 						}
 					}
 				}
-
+			lat = strtok(inchar, ",");
+			lat = strtok(NULL, ",");
+			NorS = strtok(NULL, ",");
+			lon = strtok(NULL, ",");
+			EorW = strtok(NULL, ",");
+		  fix = strtok(NULL, ",");
+				if(fix[0]=='1'){ 
+					flag=1;
+				}
 		}
-
+		flat= strtod(lat,NULL); 
+		flon= strtod(lon,NULL);
+		flat=flat/100;
+		flon=flon/100;
+		ilat=(u8)flat;
+		ilon=(u8)flon;
+		flat=flat-ilat;
+		flon=flon-ilon;
+		flat=(flat*100)/60;
+		flon=(flon*100)/60;
+		rlat=ilat+flat;
+		rlon=ilon+flon;
+		if(NorS[0] == 'S'){ 
+			rlat*=-1;
+		}
+		if(EorW[0]=='W'){ 
+			rlon*=-1;
+		}
+			arr[0]=rlat;
+			arr[1]=rlon;
 }
