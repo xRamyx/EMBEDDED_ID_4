e7d24d00751f7e76abbbd54b04596483817436f8
Remon7 <85322997+Remon7@users.noreply.github.com>
2021-06-09 20:40:44 +0200

    Update UART_5 initiallization.c

--- UART_5 initiallization.c
+++ UART_5 initiallization.c
@@ -5,14 +5,14 @@ void UART5_init (void){
 	SYSCTL_RCGCUART_R |= 0x0020;
 	while((SYSCTL_PRUART_R & 0x0020)==0){};
 	UART5_CTL_R &= ~0x0001;
-	UART5_IBRD_R = 104;
-	UART5_FBRD_R = 11;
-	UART5_LCRH_R = 0x0070;
+	UART5_IBRD_R = 104; // int(16000000/(16*9600))=104
+	UART5_FBRD_R = 11; // int(0.16667 * 64 + 0.5)=11
+	UART5_LCRH_R = 0x0070; // 8-bit word length, enable FIFO
 	UART5_CTL_R = 0x0301;
 }
 
 u8 UART5_InChar(void)
 {
-	while((UART5_FR_R & 0x10)!=0);
-	return ((u8)(UART5_DR_R & 0xFF)); 
+	while((UART5_FR_R & 0x10)!=0); //Wait if the reciever FIFO is empty
+	return ((u8)(UART5_DR_R & 0xFF)); //Returns the first 8-bit (Data)
 }
