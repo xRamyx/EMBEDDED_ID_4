fd414cd0e7fd7f571593e61bf85dcc114f776693
Remon7 <85322997+Remon7@users.noreply.github.com>
2021-06-09 14:42:29 +0200

    Update UART_5 initiallization.c

--- UART_5 initiallization.c
+++ UART_5 initiallization.c
@@ -6,7 +6,13 @@ void UART5_init (void){
 	while((SYSCTL_PRUART_R & 0x0020)==0){};
 	UART5_CTL_R &= ~0x0001;
 	UART5_IBRD_R = 104;
-	UART5_FBRD_R = 11; 
-	UART5_LCRH_R = 0x0070; 
+	UART5_FBRD_R = 11;
+	UART5_LCRH_R = 0x0070;
 	UART5_CTL_R = 0x0301;
+}
+
+u8 UART5_InChar(void)
+{
+	while((UART5_FR_R & 0x10)!=0);
+	return ((u8)(UART5_DR_R & 0xFF)); 
 }
