95085b21126a8ea83e8ccb5e6b1ef2dc54f25dd6
OmarHisham100 <85303899+OmarHisham100@users.noreply.github.com>
2021-06-09 20:30:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "GPIO_INIT.h"
 #include "tm4c123gh6pm.h"
 #include "Lcd.h"
+#include "KPD.h"
 
 int main(void)
 {
@@ -9,11 +10,29 @@ int main(void)
 	UART5_init ();
 	PortB_Init();
 	init_lcd();
+	Keypad_Init();
 	u16 buffer[10];
 	f32 tot;
 	f32 lato=0;
 	f32 lono=0;
 	f32 arr[2];
+	u8 flagsw1=0;
+	u8 dest[3];
+	lcd_write_string("Enter distance");
+	lcd_gotoxy(5,2);
+	lcd_write_string("M");
+	for(int i=0;i<3;i++){
+				dest[i]=Keypad_Getkey();
+		    delay_ms(250);
+				lcd_gotoxy(1+i,2);
+				lcd_write(dest[i]);
+			}
+	
+			u16 destination = 0;
+			for (int i = 0; i < 3; i++){ 
+				destination = (10 * destination) + (dest[i] - '0');
+			}
+			lcd_cmd(lcd_Clear);
 			
 	while (1)
 	{
@@ -26,12 +45,17 @@ int main(void)
 		  lcd_write_string(buffer);
 			lcd_gotoxy(5,2);
 			lcd_write_string("M");
-			if((SW2_Input()!=0x01)){
+			if((SW2_Input()!=0x01)  || (tot>=destination)){ 
 				RGB_LEDs((u16)tot);
-				
+				while(flagsw1==0){
+					if(SW1_Input()!=0x10){
+						flagsw1=1;
+					}
+				}
+				flagsw1=0;
 			}
 			
-			delay_ms(1000);
+			delay_ms(1000); 
 		
 	}
 		
