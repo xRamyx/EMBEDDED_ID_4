0c28ac553b8189f8529e25829ff49bf7cea0b66a
AhmedEhab99 <85303713+AhmedEhab99@users.noreply.github.com>
2021-06-10 01:27:57 +0200

    Add files via upload

---
+++ lcd.c
@@ -0,0 +1,81 @@
+#include "STD_TYPES.h"
+#include "GPIO_INIT.h"
+#include "tm4c123gh6pm.h"
+#include "Lcd.h"
+
+
+
+void init_lcd (void)                     
+{
+	
+	lcd_cmd(lcd_FunctionSet8bit);
+	delay_ms(1);
+
+	
+	
+	lcd_cmd(lcd_EntryMode);
+	delay_ms(1);
+	
+	
+	lcd_cmd(lcd_DisplayOn);
+	delay_ms(1);
+	
+
+	lcd_cmd(lcd_Clear);
+	delay_ms(2);
+	
+	
+	lcd_cmd(lcd_Home);
+	delay_ms(2);
+	
+	return;
+}
+
+
+
+void lcd_cmd(u8 cmd)            
+{
+	GPIO_PORTB_DATA_R=cmd;
+	
+	GPIO_PORTE_DATA_R=(LOW<<PIN1)|(LOW<<PIN2)|(HIGH<<PIN3);
+	delay_ms(1);
+	GPIO_PORTE_DATA_R=(LOW<<PIN1)|(LOW<<PIN2)|(LOW<<PIN3);
+	delay_ms(50);
+	
+	return;
+}
+
+
+
+
+
+void lcd_write(u8 data)
+{
+	GPIO_PORTB_DATA_R=data;
+	
+	GPIO_PORTE_DATA_R=(HIGH<<PIN1)|(LOW<<PIN2)|(HIGH<<PIN3);
+	delay_ms(1);
+	GPIO_PORTE_DATA_R=(HIGH<<PIN1)|(LOW<<PIN2)|(LOW<<PIN3);
+	delay_ms(50);
+	
+	return;
+}
+
+
+
+
+void lcd_write_string(u8 *str)         
+{
+	int		i=0;
+	while(str[i]!='\0')
+	{
+		lcd_write(str[i]);
+		i++;
+		
+	}
+	
+	delay_ms(100);
+	
+	return;
+	
+}
