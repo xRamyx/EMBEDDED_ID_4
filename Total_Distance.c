cb5d0308fd76b5da102bda08191e00b54325434e
xRamyx <85303564+xRamyx@users.noreply.github.com>
2021-06-11 14:32:49 +0200

    Update Total_Distance.c

--- Total_Distance.c
+++ Total_Distance.c
@@ -3,6 +3,7 @@
 #define PI 3.14159265359
 
 f32 calc_dist(f32 yo,f32 xo,f32 y1,f32 x1){
+	
 	f32 dy = (y1-yo)* PI/180.0; //Distance between latitudes and longitudes
 	f32 dx = (x1-xo)* PI/180.0;
 	
@@ -17,14 +18,13 @@ f32 calc_dist(f32 yo,f32 xo,f32 y1,f32 x1){
 	return rad * c * 1000; // Converting KM to M by multiplying by 1000
 }
 
-f32 Total_Distance (f32 yo ,f32 xo ,f32 y1, f32 x1) // previous and current latitude and longitude that will be obtained from the gps module
+f32 Total_Distance (f32* yo, f32* xo, f32 y1, f32 x1) // previous and current latitude and longitude that will be obtained from the gps module
 {
-	f32 Total;
-	if(xo!=0 && yo !=0) // check if this is my first starting point or not
-		{
-		Total+= calc_dist(yo,xo,y1,x1);
+	f32 Total=0;
+	if(*xo!=0 && *yo !=0){ // check if this is my first point we set it as the starting point only and don't calculate distance
+		Total = calc_dist(*yo,*xo,y1,x1);
 	}
-	xo=x1;
-	yo=y1;
+	*yo=y1; //dereferencing
+	*xo=x1;
 	return Total;
 }
