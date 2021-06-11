f9ea25f4be0c1a91adfe4092606c5ce53d13866f
saadmohamed12 <85323310+saadmohamed12@users.noreply.github.com>
2021-06-12 01:43:14 +0200

    Update KeyPad.c

--- KeyPad.c
+++ KeyPad.c
@@ -8,18 +8,18 @@ u8 symbol[N_row][N_col] = {{ '1', '2',  '3', 'A'},
                            { '*', '0',  '#', 'D'}};
 
 													 
-
+// Keypad_Init() configures PORTC and PORTA to scan keypad keys
 void Keypad_Init(void)
 {
-  SYSCTL_RCGCGPIO_R |= 0x00000005;  
-  while ((SYSCTL_PRGPIO_R&0x05) == 0){}; 
+  SYSCTL_RCGCGPIO_R |= 0x00000005;        //Enable clock for PORTA and PORTC  
+  while ((SYSCTL_PRGPIO_R&0x05) == 0){};  //Wait for clock to be setted
 	GPIO_PORTA_LOCK_R = 0x4C4F434B;
 	GPIO_PORTC_LOCK_R = 0x4C4F434B;
-  GPIO_PORTC_CR_R |= 0xF0;           
-  GPIO_PORTA_CR_R |= 0x3C;             
-	GPIO_PORTC_DIR_R |= 0xF0;     
-  GPIO_PORTA_DIR_R = 0x00;             
-  GPIO_PORTA_PDR_R = 0x3C;            
+  GPIO_PORTC_CR_R |= 0xF0;             //Allow settings for pins (4,5,6,7) of PORTC
+  GPIO_PORTA_CR_R |= 0x3C;             //Allow settings for pins (2,3,4,5) of PORTA
+	GPIO_PORTC_DIR_R |= 0xF0;             //PC4-PC7 are used with col and set them as digital output pins
+  GPIO_PORTA_DIR_R = 0x00;             //PA2-PA5 are used with row and set them as digital input pins
+  GPIO_PORTA_PDR_R = 0x3C;             //Enable pull down resistor on PORTA for rows
   GPIO_PORTC_DEN_R |= 0xF0;
   GPIO_PORTA_DEN_R = 0x3C;
 	GPIO_PORTA_AMSEL_R = 0x00;
@@ -27,18 +27,30 @@ void Keypad_Init(void)
 	GPIO_PORTA_PCTL_R = 0x00000000;
 }
 
+//Keypad_getkey() function returns the value of key pressed by the user
 u8 Keypad_Getkey(void)
 {
   while(1)
   {
-    for(int i = 0; i < N_col; i++)  
+    for(int i = 0; i < N_col; i++)    //Scan columns loop
     {
       GPIO_PORTC_DATA_R = (1 << i+4);
-      for(int j = 0; j < N_row; j++)  
+      delay_us(2);
+      for(int j = 0; j < N_row; j++)  //Scan rows loop
       {
         if((GPIO_PORTA_DATA_R & 0x3C) & (1 << j+2))
           return symbol[j][i];
       }
     }
   }
+}
+
+// Micro seconds delay function
+void delay_us(int t)
+{
+ int i,j;
+ for(i=0;i<t;i++)
+ for(j=0;j<3;j++)
+ {}
+ 
 }
