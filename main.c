f896f1fd9cea8fecfc69d9d3cc451795e8ac6b71
Moamen50 <85317901+Moamen50@users.noreply.github.com>
2021-06-12 23:42:29 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,13 +11,13 @@ int main(void)
 	PortB_Init();
 	init_lcd();
 	Keypad_Init();
-	u16 buffer[10];
-	f32 tot;
-	f32 lato=0;
-	f32 lono=0;
+	u16 buffer[10]; //a buffer used to store a string to be displayed on LCD
+	f32 tot=0; //variable to store total distance
+	f32 lato=0; //variable to store latitude
+	f32 lono=0; // variable to store longitude
 	f32 arr[2]; // arr[0] for latitude and arr[1] for longitude
-	u8 flagsw1=0;
-	u8 dest[3];
+	u8 flagsw1=0; // a flag used to check if switch 1 is pressed or not
+	u8 dest[3]; //an array used to store the entered distance through keypad
 	lcd_write_string("Enter distance");
 	lcd_gotoxy(5,2);
 	lcd_write_string("M");
@@ -37,15 +37,15 @@ int main(void)
 	while (1)
 	{
 			Parsing(arr);
-	  	tot=Total_Distance(lato,lono ,arr[0], arr[1]);
+	  	tot+= Total_Distance(&lato, &lono , arr[0], arr[1]);
 			lcd_gotoxy(1,1);
 			lcd_write_string("Distance reached");
 			lcd_gotoxy(1,2);
-			sprintf(buffer,"%d",(u16)tot); // Change integer to string
+			sprintf(buffer,"%d",(u16)tot); // Change integer to string and making explicit type casting from float to integer
 		  lcd_write_string(buffer);
 			lcd_gotoxy(5,2);
 			lcd_write_string("M");
-			if((SW2_Input()!=0x01)  || (tot>=destination)){ // If switch2 is pressed >> 0x00, Turn on LEDs and stop adding new points
+			if((SW2_Input()!=0x01 || (tot>=destination))){ // If switch2 is pressed >> 0x00, Turn on LEDs and stop adding new points or if distance equals to the pre-defined distance
 				RGB_LEDs((u16)tot);
 				while(flagsw1==0){
 					if(SW1_Input()!=0x10){ // If switch1 is pressed >> 0x00, Continue adding new points
