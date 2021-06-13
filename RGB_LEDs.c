802bf69d71f79a7ddbb539ce2d22d6ccaec62c8c
xRamyx <85303564+xRamyx@users.noreply.github.com>
2021-06-13 15:10:09 +0200

    Update and rename RED_LED.c to RGB_LEDs.c

--- RGB_LEDs.c
+++ RGB_LEDs.c
@@ -2,6 +2,6 @@
 #include "tm4c123gh6pm.h"
 void RGB_LEDs(u16 Total_Dist){
 	if(Total_Dist > 100){
-		GPIO_PORTF_DATA_R |= 0x02; // Make RED LED light up
+		GPIO_PORTF_DATA_R |= 0x02; // Make RGB LEDs light up
  }
 }
